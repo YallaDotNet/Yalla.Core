6cf13912aee14675c7a53b46cf1ea8596653a74b
Dmitry Shechtman <damnian@gmail.com>
2015-11-27 21:02:10 +0200

    Add NamespaceDoc

---
+++ src/Yalla.Core/Portable/NamespaceDoc.cs
@@ -0,0 +1,10 @@
+﻿namespace Yalla
+{
+    /// <summary>
+    /// Yalla Core classes, interfaces and enumerations.
+    /// </summary>
+    [System.Runtime.CompilerServices.CompilerGenerated]
+    internal sealed class NamespaceDoc
+    {
+    }
+}
