f29f8ebc5c7224360b85970f80480e92230e3693
Dmitry Shechtman <damnian@gmail.com>
2015-11-30 18:28:23 +0200

    Extract YallaAssemblyInfo.cs

---
+++ src/Properties/YallaAssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Resources;
+using System.Reflection;
+
+[assembly: AssemblyCompany("Dmitry Shechtman")]
+[assembly: AssemblyProduct("YALLA.NET")]
+[assembly: AssemblyCopyright("Copyright © Dmitry Shechtman 2015")]
+[assembly: NeutralResourcesLanguage("en")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
--- src/Yalla.Core/Portable/Properties/AssemblyInfo.cs
+++ src/Yalla.Core/Portable/Properties/AssemblyInfo.cs
@@ -1,21 +1,9 @@
-﻿using System.Resources;
+﻿using System;
 using System.Reflection;
-using System;
 using System.Runtime.CompilerServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Yalla.Core")]
+[assembly: AssemblyTitle("YALLA.NET Core")]
 [assembly: AssemblyDescription("Yet Another Logging Library Abstraction for .NET (Core Library)")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Dmitry Shechtman")]
-[assembly: AssemblyProduct("YALLA.NET")]
-[assembly: AssemblyCopyright("Copyright © Dmitry Shechtman 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
-
 [assembly: AssemblyVersion("1.0.*")]
 [assembly: CLSCompliant(true)]
 
